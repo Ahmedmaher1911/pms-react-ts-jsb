dac2e47edee660e23564d835dcdad84c57423449
MohammadSObeidat <mohammadsameerobeidat@gmail.com>
2024-12-04 16:54:28 +0300

    login

--- src/modules/authentication/components/Login/Login.tsx
+++ src/modules/authentication/components/Login/Login.tsx
@@ -2,7 +2,7 @@ import styles from "./Login.module.css";
 import imgLogo from "./../../../../assets/images/PMSLogo.svg";
 import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import { AUTH_URL, axiosInstanceUrl } from "../../../../services/EndPoints";
+import { AUTH_URL, axiosInstanceURL } from "../../../../services/EndPoints";
 import { useState } from "react";
 import { EMAIL_VALIDATION, PASWORD_VALIDATION } from '../../../../services/Validation/Validation';
 
@@ -22,7 +22,7 @@ export default function Login() {
 
   const onSubmit = async (data: loginForm) => {
     try {
-      const response = await axiosInstanceUrl.post(AUTH_URL.LOGIN, data);
+      const response = await axiosInstanceURL.post(AUTH_URL.LOGIN, data);
       console.log(response);
       navigate("/dashboard")
     } catch (error) {
